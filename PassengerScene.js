0721336dca86160cbe26f883568093e2bf6bb03c
sammydavisJR <whatever@thatsgreat.thank>
2016-10-28 16:31:57 +0000

    different alignment for ancillaries, box around go back

--- PassengerScene.js
+++ PassengerScene.js
@@ -7,19 +7,20 @@ export default class PassengerScene extends Component {
     var passenger = this.props.passenger;
     var psInfo = passenger.passenger;
     return (
-      <ScrollView>
+      <ScrollView style={styles.scrollView}>
         <TouchableHighlight style={{paddingTop:20}} onPress={ () => this.props.navigator.pop() }>
-          <Text>GO Back</Text>
+          <View>
+            <Text style={styles.button}>GO Back</Text>
+          </View>
         </TouchableHighlight>
         <View style={styles.rowContainer}>
-          <PassengerTextField fieldInfo="Title" fieldValue={psInfo.title}/>
-          <PassengerTextField fieldInfo="FirstName" fieldValue={psInfo.first_name}/>
-          <PassengerTextField fieldInfo="LastName" fieldValue={psInfo.last_name}/>
-          <PassengerTextField fieldInfo="Seat#" fieldValue={passenger.seat_row + passenger.seat_column}/>
+          <PassengerTextField fieldInfo="Name" fieldValue={psInfo.title + " " + psInfo.first_name + " " + psInfo.last_name}/>
         </View>
         <View style={styles.rowContainer}>
           <PassengerTextField fieldInfo="Age" fieldValue={psInfo.age}/>
           <PassengerTextField fieldInfo="Birthday" fieldValue={psInfo.birthday}/>
+                    <PassengerTextField fieldInfo="Seat #" fieldValue={passenger.seat_row + passenger.seat_column}/>
+
         </View>
         <View style={styles.rowContainer}>
           <PassengerTextField fieldInfo="Nationality" fieldValue={psInfo.nationality}/>
@@ -36,16 +37,22 @@ export default class PassengerScene extends Component {
             <Image style={styles.image} source={require('./img/personIcon.png')} />
           </View>
         </View>
-        <Text style={styles.fieldValue}>Special services</Text>
-        <View style={styles.container}>{psInfo.SSR.map(function(item,i){
-                                       return (<PaxSSR SSR={item} key={i}/>)
-                                   })}
-                </View>
-        <Text style={styles.fieldValue}>Ancillaries</Text>
-        <View>
-        {psInfo.Ancillaries.map(function(item,i){
-                                               return (<PaxAncillaries Ancillaries={item} key={i}/>)
+        <View style={styles.rowContainer, styles.listBoxContainer}>
+            <View style={styles.container, styles.startInParent}>
+                <Text style={styles.fieldValue}>Special services</Text>
+                <View>{psInfo.SSR.map(function(item,i){
+                                               return (<PaxSSR SSR={item} key={i}/>)
                                            })}
+                </View>
+            </View>
+            <View style={styles.container, styles.startInParent}>
+                <Text style={styles.fieldValue}>Ancillaries</Text>
+                        <View>
+                        {psInfo.Ancillaries.map(function(item,i){
+                                                               return (<PaxAncillaries Ancillaries={item} key={i}/>)
+                                                           })}
+                </View>
+            </View>
         </View>
       </ScrollView>
     )
@@ -86,6 +93,17 @@ class PaxAncillaries extends Component{
 }*/
 
 const styles = StyleSheet.create({
+    button:{
+        borderRadius: 4,
+        borderWidth: 0.5,
+        width:120,
+        fontWeight:'bold',
+        padding:5,
+        margin:5
+        },
+    scrollView:{
+    padding:1
+    },
   fieldInfo: {
     fontSize: 10,
   },
@@ -110,6 +128,9 @@ const styles = StyleSheet.create({
     height: 80,
     width: 80
   },
+  startInParent:{
+    justifyContent:'flex-start'
+  },
   centerInParent: {
     justifyContent:'center',
   },
@@ -118,6 +139,12 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between', 
     paddingHorizontal: 30,
     paddingVertical: 20
+  },
+  listBoxContainer:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingHorizontal: 20,
+      paddingVertical: 10
   }
 });
 
